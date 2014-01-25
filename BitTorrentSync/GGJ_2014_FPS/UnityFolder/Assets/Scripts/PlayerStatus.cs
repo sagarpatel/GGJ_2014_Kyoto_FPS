907f9f795fdf804c0a2e4c818ca28adc2f3683a1
Sagar Patel <17.sagar.patel@gmail.com>
2014-01-26 06:38:40 +0900

    Fujii's changes

---
+++ BitTorrentSync/GGJ_2014_FPS/UnityFolder/Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStatus : MonoBehaviour {
+
+	public bool isGetKey = false;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		isGetKey = false;
+	}
+	
+	void OnGUI(){
+		if(isGetKey)
+			GUI.Label(new Rect (10, 10, 100, 20), "I have a key.");
+	}
+
+	void GetKey(){
+		isGetKey = true;
+	}
+
+}
