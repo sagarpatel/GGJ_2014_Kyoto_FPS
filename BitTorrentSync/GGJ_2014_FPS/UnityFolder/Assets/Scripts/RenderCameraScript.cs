30a6da6db6bb893846a83bc7b5aada2051cc5181
Sagar Patel <17.sagar.patel@gmail.com>
2014-01-25 23:21:36 +0900

    Tweaks to prefabs, added materials to camera

--- BitTorrentSync/GGJ_2014_FPS/UnityFolder/Assets/Scripts/RenderCameraScript.cs
+++ BitTorrentSync/GGJ_2014_FPS/UnityFolder/Assets/Scripts/RenderCameraScript.cs
@@ -14,6 +14,8 @@ public class RenderCameraScript : MonoBehaviour
 		renderTexture =  new RenderTexture(textureWidth, textureHeight, 16, RenderTextureFormat.ARGB32);
 		renderCamera = gameObject.GetComponent<Camera>();
 		renderCamera.targetTexture = renderTexture;
+		Vector3 parentScale = transform.parent.localScale;
+		transform.localScale = new Vector3(transform.localScale.x/parentScale.x , transform.localScale.y/parentScale.y, transform.localScale.z/parentScale.z);
 	}
 	
 	// Update is called once per frame
